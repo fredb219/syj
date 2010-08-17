208a09a18b0212eecb94b74fd63840e6fcce8878
arno <arno@renevier.net>
2010-08-17 22:44:56 +0200

    faq

---
+++ application/controllers/FaqController.php
@@ -0,0 +1,12 @@
+<?php
+/*  This file is part of Syj, Copyright (c) 2010 Arnaud Renevier,
+    and is published under the AGPL license. */
+
+class FaqController extends Zend_Controller_Action
+{
+    public function indexAction() {
+        $this->view->headLink()->appendStylesheet('css/generic.css');
+        $this->view->headLink()->appendStylesheet('css/faq.css');
+        $this->view->headTitle($this->view->translate("Frequently asked questions"));
+    }
+}
