e1cf73aef18917982f7d748487871b4b266d94df
arno <arno@renevier.net>
2010-09-27 09:50:18 +0200

    add japanese locale

--- scripts/updatepo.sh
+++ scripts/updatepo.sh
@@ -4,7 +4,7 @@ GITROOT=`git rev-parse --show-cdup`
 GITROOT=${GITROOT:-.}
 APPROOT=$GITROOT/application
 find $APPROOT -name '*.php' -o -name '*.phtml' | xgettext -L php --keyword=translate --keyword=__ --from-code=UTF-8 -o $POTFILE -f -
-for lang in en fr; do
+for lang in en fr ja; do
     echo $lang language
     msgmerge -U $APPROOT/languages/lang_${lang}.po $POTFILE
     msgfmt --statistics -o $APPROOT/languages/lang_${lang}.mo $APPROOT/languages/lang_${lang}.po
